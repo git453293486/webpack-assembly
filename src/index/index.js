7c16166d90b65463ca1d29352d8625a6af276aa0
红枫花 <l>
2018-11-09 19:26:19 +0800

    first committ

---
+++ src/index/index.js
@@ -0,0 +1,102 @@
+
+import '../assets/css/webset.scss';
+
+import layer from 'layui-layer';
+
+import '../assets/js/custom.js'
+
+// import Vue from 'vue';
+// import Swipercss from '../js/plugin/swiper/swiper.min.css'
+// import Swiper from 'swiper'
+
+
+// function add(...x){
+//     return x.reduce((m,n)=>m+n);
+// }
+// function add(...x){
+//     return x.reduce(function(m,n){
+//         return m+n;
+//     })
+// }
+
+
+
+
+// var ap = function(x, y) { 
+//     x++;
+//     y--;
+//     return x * y;
+// };
+// var ap = x => x+1;
+// alert(ap(1));
+
+
+
+
+
+
+
+
+
+// import {m,square,add} from  './public/public.js'
+// console.log(square(111));
+// console.log(add (2, 5))
+// alert(m);
+
+
+
+
+
+
+
+
+//var people = name => 'hello' + name
+// var  people = function(name){
+//     return ('hello'+name);
+// } 
+// alert(people(1))
+
+
+
+
+// import '../js/plugin/pa/partialviewslider.css'
+// import '../js/plugin/pa/partialviewslider.js';
+// $(document).ready(function(){
+//     $('#partial-view').partialViewSlider();
+// });   
+
+// $('button').click(function(){
+//     alert(1);
+// })
+// window.onload=function(){
+//     new Swiper ('.swiper-container', {
+//         direction: 'horizontal', // 垂直切换选项
+//         loop: true, // 循环模式选项
+//         speed:1000,
+//         autoplay : {
+//             delay:2500,
+//             disableOnInteraction: false,
+//         },
+//         // 如果需要分页器
+//         pagination: {
+//         el: '.swiper-pagination',
+//         },
+//         // 如果需要前进后退按钮
+//         navigation: {
+//             nextEl: '.swiper-button-next',
+//             prevEl: '.swiper-button-prev',
+//         },
+//     })
+// }
+
+// new Vue({
+//     el:'#app',
+//     data:{
+//         mess:"asdasd"
+//     },
+//     methods:{
+//         asd:function(){
+            
+//         }
+//     }
+// })
